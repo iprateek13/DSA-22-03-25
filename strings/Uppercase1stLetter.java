b04a266c204edfe75ef2661bcfef32e361401e14
Prateek gupta <guptaji233001@gmail.com>
2026-04-13 23:59:47 +0530

    Add Uppercase1stLetter class to capitalize first letter of each word in a string

---
+++ strings/Uppercase1stLetter.java
@@ -0,0 +1,35 @@
+package strings;
+
+public class Uppercase1stLetter {
+
+    public static String toUppercase(String str) {
+
+        StringBuilder sb = new StringBuilder(""); // result banane ke liye
+
+        char ch = Character.toUpperCase(str.charAt(0)); // first char uppercase
+        sb.append(ch); // add first char
+
+        for (int i = 1; i < str.length(); i++) {
+
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+
+                sb.append(str.charAt(i)); // space bhi add karna hai
+
+                i++; // next char pe jump (new word start)
+
+                sb.append(Character.toUpperCase(str.charAt(i))); // next char uppercase
+
+            } else {
+
+                sb.append(str.charAt(i)); // normal char add
+            }
+        }
+
+        return sb.toString(); // final string return
+    }
+
+    public static void main(String[] args) {
+        String str = "hi, i am prateek";
+        System.out.println(toUppercase(str));
+    }
+}
