f1c3319a89f450aa5095e4354111665be3f8cf65
Prateek gupta <guptaji233001@gmail.com>
2026-04-13 20:20:06 +0530

    Add SotedDist class to calculate shortest distance based on directional input 13-02

---
+++ strings/SotedDist.java
@@ -0,0 +1,31 @@
+package strings;
+
+public class SotedDist {
+    public static int sortDist(String path) {
+        int x = 0, y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            if (dir == 'N') {
+                y++;
+            }
+            if (dir == 'S') {
+                y--;
+            }
+            if (dir == 'E') {
+                x++;
+            }
+            if (dir == 'w') {
+                x--;
+            }
+        }
+        int X2 = x * x;
+        int Y2 = y * y;
+        return (int) Math.sqrt(X2 + Y2);
+    }
+
+    public static void main(String[] args) {
+        String path = "WNEENESENNN";
+        int sortDist = sortDist(path);
+        System.out.println("Sortest DIst is : " + sortDist);
+    }
+}
