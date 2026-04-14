68c26e58eb715d0550a05c868855205d174aa90b
Prateek gupta <guptaji233001@gmail.com>
2026-04-14 22:51:15 +0530

    Add OddEvenCheck class to determine if a number is odd or even 14-04

---
+++ bitmanipulation/OddEvenCheck.java
@@ -0,0 +1,20 @@
+package bitmanipulation;
+
+import java.util.Scanner;
+
+public class OddEvenCheck {
+    public static void checkEvenOdd(int n) {
+
+        if ((n & 1) == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        checkEvenOdd(n);
+    }
+}
