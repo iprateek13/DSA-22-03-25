ea91e00d9c12812de4a74fe78f3c0a1dc81bcace
Prateek gupta <guptaji233001@gmail.com>
2026-04-16 01:15:40 +0530

    Add ClearIbits class for clearing specified bits in an integer

---
+++ bitmanipulation/ClearIbits.java
@@ -0,0 +1,23 @@
+package bitmanipulation;
+
+import java.util.Scanner;
+
+public class ClearIbits {
+    public static int cleariBits(int n, int i) {
+        int bitmask = ~0 << i;
+        return n & bitmask;
+    
+    }
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = sc.nextInt();
+        int result = cleariBits(n, i);
+        if (result == n) {
+            System.out.println("Already Cleared");
+        } else {
+            System.out.println("Last i Bits Now Cleared");
+        }
+
+    }
+}
