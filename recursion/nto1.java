fab7ec44c1157befc8d346d6a70db61db703fe78
Prateek gupta <guptaji233001@gmail.com>
2026-04-23 11:38:06 +0530

    functionality for printing numbers from n to 1

--- recursion/nto1.java
+++ recursion/nto1.java
@@ -1,7 +1,29 @@
 package recursion;
 
-public class nto1 {
+import java.util.Scanner;
+
+public class Nto1 {
+    public static void nto1print(int n) {
+        if (n == 0)
+            return;
+        System.out.print(n + " ");
+        nto1print(n - 1);
+    }
+
+    public static void nto1pri(int n, int i) {
+        if (i > n) {
+            return;
+        }
+        nto1pri(n, i + 1);
+        System.out.print(i+" ");
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int n = sc.nextInt();
+        // nto1print(n);
+        nto1pri(n, 1);
+        sc.close();
     }
 }
