c3edce5c18f9c196bf5321cd67c37728784e29bd
Prateek gupta <guptaji233001@gmail.com>
2026-04-23 23:03:38 +0530

    Refactor lastOccur1 method for clarity and update main method to test last occurrence of target

--- recursion/LastOccur.java
+++ recursion/LastOccur.java
@@ -16,9 +16,22 @@ public class LastOccur {
 
     }
 
+    public static int lastOccur1(int[] arr, int i, int target) {
+        if (i == arr.length) {
+            return -1;
+        }
+        int lastOccur = lastOccur(arr, i + 1, target);
+        if (lastOccur == -1 && arr[i] == target) {
+            return i;
+        }
+        return lastOccur;
+
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 2, 3, 4, 3, 5, 3, 5, 4 };
-        int index = lastOccur(arr, 0, 2);
+        int arr[] = { 2, 3, 4, 3, 5, 4 };
+        int index = lastOccur(arr, 0, 3);
+        System.out.println(lastOccur1(arr, 0, 3));
         System.out.println("index is : " + index);
     }
 }
